698f676f457ed2994e02fa8ce599c638523ea5ad
Smagic Jones <>
2023-09-06 09:35:15 +0100

    tidying run-seed.js

--- db/run-seed.js
+++ db/run-seed.js
@@ -1,15 +1,7 @@
-// const {seed} = require('./seed')
+
 
 import seed from './seed.js'
 import db from './connection.js'
-// const db = require('./connection')
-
-
-// const supporters = fetchSupporters = () => {
-//     return fetch('https://www.few-far.co/api/techtest/v1/supporters', {
-//         method: "GET",
-//     }).then((res)=> res.json())
-// }
 
 
 const runSeed = () => {
